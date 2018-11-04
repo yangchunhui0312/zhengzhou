3c7b7c97df843c433c58df183b63624732ecc4f5
yangchunhui0312 <369674186@qq.com>
2018-11-04 19:35:54 +0800

    第一次上传

---
+++ src/main/java/com/henu/main/ZhengzhouApplication.java
@@ -0,0 +1,12 @@
+package com.henu.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhengzhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhengzhouApplication.class, args);
+    }
+}
